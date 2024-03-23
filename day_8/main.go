92f5fa38601505c0d756cf1d93479778232977e1
Frank Hmeidan <frank.hmeidan@gmail.com>
2024-03-23 15:51:51 -0400

    progress...

--- day_8/main.go
+++ day_8/main.go
@@ -1,1 +1,38 @@
 package main
+
+import (
+	"bufio"
+	"day_8/network"
+	"fmt"
+	"os"
+	"path"
+)
+
+const inputFile = "/Users/frankhmeidan/golang/advent_of_code/day_8/input.txt"
+
+func fileScanner() (*bufio.Scanner, *os.File) {
+	filePath := path.Join(inputFile)
+	file, err := os.Open(filePath)
+	if err != nil {
+		panic("could not open file")
+	}
+
+	scanner := bufio.NewScanner(file)
+	if err != nil {
+		panic("could not scan file")
+	}
+
+	return scanner, file
+}
+
+func main() {
+	scanner, file := fileScanner()
+	defer file.Close()
+
+	maze, err := network.NewNetwork(scanner)
+	if err != nil {
+		panic("could not build network")
+	}
+	steps := maze.StepsToFinish()
+	fmt.Printf("steps to finish: %d\n", steps)
+}
--- day_8/network/network.go
+++ day_8/network/network.go
@@ -1,22 +1,95 @@
 package network
 
-import "bufio"
+import (
+	"bufio"
+	"fmt"
+	"github.com/samber/lo"
+	"strings"
+)
+
+const End = "ZZZ"
 
 type Node struct {
 	label string
-	left  *Node
-	right *Node
+	left  string
+	right string
 }
 
 type Network struct {
 	directions []byte
 	nodes      []*Node
+	lookup     map[string]*Node
 }
 
 func NewNetwork(s *bufio.Scanner) (*Network, error) {
-	return nil, nil
+	getDirections := func(dirStr string) []byte {
+		return []byte(dirStr)
+	}
+	getNodes := func(lines []string) []*Node {
+		return lo.Map(lines, func(s string, i int) *Node {
+			pieces := strings.Split(s, "=")
+			label := strings.Trim(pieces[0], " ")
+			next := strings.Trim(pieces[1], " ()")
+			leftAndRight := strings.Split(next, ",")
+			left, right := strings.Trim(leftAndRight[0], " "), strings.Trim(leftAndRight[1], " ")
+			return &Node{
+				label: label,
+				left:  left,
+				right: right,
+			}
+		})
+	}
+	getLookup := func(nodes []*Node) map[string]*Node {
+		lookup := make(map[string]*Node)
+		for _, node := range nodes {
+			lookup[node.label] = node
+		}
+		return lookup
+	}
+
+	var lines []string
+	for s.Scan() {
+		line := s.Text()
+		lines = append(lines, line)
+	}
+	directions := getDirections(lines[0])
+	nodes := getNodes(lines[2:])
+	lookup := getLookup(nodes)
+
+	return &Network{
+		directions: directions,
+		nodes:      nodes,
+		lookup:     lookup,
+	}, nil
+
 }
 
-func (n *Network) StepsToFinish() int {
-	return 0
+func (n *Network) StepsToFinish() int64 {
+	steps := int64(0)
+	currNode := n.nodes[0]
+	exitFound := false
+	fmt.Println("directions: %s", n.directions)
+	fmt.Println("")
+	for !exitFound {
+		for _, curr := range n.directions {
+			switch curr {
+			case 'L':
+				currNode = n.lookup[currNode.left]
+			case 'R':
+				currNode = n.lookup[currNode.right]
+			default:
+				panic("direction not left or right")
+			}
+			steps += 1
+			if steps%1e6 == 0 {
+				fmt.Printf("currLabel: %s, steps: %d\n", currNode.label, steps)
+			}
+
+			if currNode.label == "ZZZ" {
+				exitFound = true
+				break
+			}
+		}
+	}
+	return steps
 }
